e5e87c95f01150c21590f7f72d4c926fa85308e4
SafalTammewar <safal.tammewar@compumatrice.com>
2019-05-06 13:19:01 +0530

    code

--- server.js
+++ server.js
@@ -55,8 +55,9 @@ apiRouter.use(`/ping`, pingRoute);
 swaggerTools.initializeMiddleware(swaggerSpec, (swaggerMiddleware) => {
   swaggerSpec.basePath = `/${process.env.APIVERSION}/api`;
   apiRouter.use(swaggerMiddleware.swaggerUi({
-    apiDocs: `/api-docs`,
-    swaggerUi: `/docs`
+    swaggerUi: `/docs`,
+    apiDocs: `/api-docs`
+    
   }));
 
   logger.info(`Swagger UI is serving at: /${process.env.APIVERSION}/docs`);
