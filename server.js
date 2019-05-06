8219c3b61a2563cc97e686d5491b14df80ddd1cd
SafalTammewar <safal.tammewar@compumatrice.com>
2019-05-06 15:03:52 +0530

    code changes

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ apiRouter.use(`/ping`, pingRoute);
 
 swaggerTools.initializeMiddleware(swaggerSpec, (swaggerMiddleware) => {
   swaggerSpec.basePath = `/${process.env.APIVERSION}/api`;
-  apiRouter.use(swaggerMiddleware.swaggerUi({
+  apiRouter.use(mount,swaggerMiddleware.swaggerUi({
     apiDocs: `/api-docs`,
     swaggerUi: `/docs`,
     
